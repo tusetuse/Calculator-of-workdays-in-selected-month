a233909a430812382c555d478373624f97e581e4
_tuse <75646496+tusetuse@users.noreply.github.com>
2024-05-03 22:50:56 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Pocet_pracovnych_dni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Vypočítať = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Vypočítať
+            // 
+            this.Vypočítať.AccessibleName = "buttonCalculate";
+            this.Vypočítať.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Vypočítať.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Vypočítať.Location = new System.Drawing.Point(364, 159);
+            this.Vypočítať.Name = "Vypočítať";
+            this.Vypočítať.Size = new System.Drawing.Size(130, 29);
+            this.Vypočítať.TabIndex = 0;
+            this.Vypočítať.Text = "Vypočítať";
+            this.Vypočítať.UseVisualStyleBackColor = true;
+            this.Vypočítať.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(231, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(449, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mesiac";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rok";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(215, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Počet pracovných dní v danom mesiaci je:  ";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(228, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(266, 55);
+            this.label5.TabIndex = 7;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Vypočítať);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "Výpočet počtu pracovných dní v danom mesiaci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Vypočítať;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Pocet_pracovnych_dni
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<string> pravidelneSviatky;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pravidelneSviatky = new List<string>()
+            {
+                 "06.01",
+                 "01.05",
+                 "08.05",
+                 "15.09",
+                 "01.11",
+                 "24.12",
+                 "25.12",
+                 "26.12"
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rok;
+            int mesiac;
+
+            if (int.TryParse(textBox2.Text, out rok) && int.TryParse(textBox1.Text, out mesiac) && mesiac <= 12 && mesiac >= 1 && rok >= 1)
+            {
+                int pracovneDni = PocitajPracovneDni(rok, mesiac);
+
+                label1.Text = $" {pracovneDni}";
+            }
+            else
+            {
+                MessageBox.Show("Prosím zadajte mesiac a rok v správnom číselnom formáte.");
+            }
+        }
+
+        private int PocitajPracovneDni(int rok, int mesiac)
+        {
+            int pracovneDni = 0;
+            DateTime velkonocnyPiatok = VelkonocnyPiatok(rok, mesiac);
+            DateTime velkonocnyPondelok = VelkonocnyPondelok(rok, mesiac);
+
+            for (int den = 1; den <= DateTime.DaysInMonth(rok, mesiac); den++)
+            {
+                DateTime date = new DateTime(rok, mesiac, den);
+                if (JePracovnyDen(date, velkonocnyPiatok, velkonocnyPondelok))
+                {
+                    pracovneDni++;
+                }
+            }
+            return pracovneDni;
+        }
+
+        private bool JePracovnyDen(DateTime datum, DateTime velkonocnyPiatok, DateTime velkonocnyPondelok)
+        {
+            return datum.DayOfWeek != DayOfWeek.Saturday &&
+                   datum.DayOfWeek != DayOfWeek.Sunday &&
+                   datum != velkonocnyPiatok &&
+                   datum != velkonocnyPondelok &&
+                   !pravidelneSviatky.Contains(datum.ToString("dd.MM"));
+        }
+
+        private DateTime VelkonocnyPiatok(int rok, int mesiac)
+        {
+            int a = rok % 19;
+            int b = rok % 4;
+            int c = rok % 7;
+            int m = 24;
+            int n = 5;
+            int d = (19 * a + m) % 30;
+            int e = (n + 2 * b + 4 * c + 6 * d) % 7;
+            int nedelaMarec = 22 + d + e;
+            int nedelaApril = d + e - 9;
+
+            if (nedelaMarec >= 0 && nedelaMarec <= 31)
+            {
+                return new DateTime(rok, 3, nedelaMarec).AddDays(-2);
+            }
+            else if (nedelaMarec == 26 && d == 29 && (11 * (rok % 19) + 11) % 30 < 19)
+            {
+                return new DateTime(rok, 3, 25).AddDays(-2);
+            }
+            else if (nedelaApril > 0 && nedelaApril <= 31)
+            {
+                return new DateTime(rok, 4, nedelaApril).AddDays(-2);
+            }
+            else
+            {
+                return DateTime.MinValue;
+            }
+        }
+
+        private DateTime VelkonocnyPondelok(int rok, int mesiac)
+        {
+            int a = rok % 19;
+            int b = rok % 4;
+            int c = rok % 7;
+            int m = 24;
+            int n = 5;
+            int d = (19 * a + m) % 30;
+            int e = (n + 2 * b + 4 * c + 6 * d) % 7;
+            int nedelaMarec = 22 + d + e;
+            int nedelaApril = d + e - 9;
+
+            if (nedelaMarec >= 0 && nedelaMarec <= 31)
+            {
+                return new DateTime(rok, 3, nedelaMarec).AddDays(1);
+            }
+            else if (nedelaMarec == 26 && d == 29 && (11 * (rok % 19) + 11) % 30 < 19)
+            {
+                return new DateTime(rok, 3, 25).AddDays(1);
+            }
+            else if (nedelaApril > 0 && nedelaApril <= 31)
+            {
+                return new DateTime(rok, 4, nedelaApril).AddDays(1);
+            }
+            else
+            {
+                return DateTime.MinValue;
+            }
+        }
+    }
+}
